6f2d0d87a0eea527981aa3dad6efe326d118aa9f
ZakhTania <tatizarova@gmail.com>
2023-11-08 18:28:33 +0300

    Вёрстка постов с API

--- components/posts-page-component.js
+++ components/posts-page-component.js
@@ -106,4 +106,38 @@ export function renderPostsPageComponent({ appEl }) {
       });
     });
   }
+
+  const listElements = document.querySelector(".posts");
+
+  const postsHtml = posts
+    .map((post) => {
+      return `  <li class="post">
+                  <div class="post-header" data-user-id=${post.user.id}>
+                      <img src=${post.user.imageUrl} class="post-header__user-image">
+                      <p class="post-header__user-name">${post.user.name}</p>
+                  </div>
+                  <div class="post-image-container">
+                    <img class="post-image" src=${post.imageUrl}>
+                  </div>
+                  <div class="post-likes">
+                    <button data-post-id=${post.user.id} class="like-button">
+                      <img src="./assets/images/like-active.svg">
+                    </button>
+                    <p class="post-likes-text">
+                      Нравится: <strong>${post.likes.length}</strong>
+                    </p>
+                  </div>
+                  <p class="post-text">
+                    <span class="user-name">${post.user.name}</span>
+                    ${post.description}
+                  </p>
+                  <p class="post-date">
+                  ${post.createdAt}
+                  </p>
+                </li>`;
+    })
+    .join("");
+
+  listElements.innerHTML = postsHtml;
+
 }
